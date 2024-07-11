99874cd1fb48c45a490b74ed8650e02d317325e5
MPAQ-Admin <admin@mpaq.org>
2024-07-11 09:49:17 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<HTML>
+	<HEAD>
+		<TITLE>The Follow Back</TITLE>
+	</HEAD>
+	<BODY>
+<?PHP
+date_default_timezone_set('America/Chicago');
+ini_set('display_errors',1);ini_set('display_startup_errors',1);error_reporting(E_ALL);
+include('/var/www/00_00/.config_cli.php');
+$user_id=$D=NULL;
+if (!isset($_POST['D'])){
+	echo "<CENTER>Welcome to &#34;The Follow Back&#34; List<BR>
+		When you enter your full address below, we will try to get all public information about who you follow and those who follow you.<BR>
+		For this to allow you to follow back, you must be logged into your account in a web browser.<BR>
+		The color codes: <FONT COLOR='green'>Green: Mutual</FONT>, <FONT COLOR='red'>Red: Not Mutual</FONT>, <FONT COLOR='black'>Black: Local</FONT><BR>	
+		<form action='/follow/".$D."' method='post'>
+			<input
+				type='url'
+				name='D'
+				id='D'
+				placeholder='https://beamship.mpaq.org/@admin'
+				size=50
+				maxlength=50
+				autofocus
+				required
+			/>
+			<button type='submit' name='request' value='".$D."'>Request</button>
+		</form>
+		</CENTER>
+";
+exit;
+// https://pixelfed.social/@justbob	Fail
+// https://kolektiva.social/@mahaska	Fail
+// https://pagan.plus/@occupyjourney	Good
+// https://beamship.mpaq.org/@sr_rss	Good
+// https://beamship.mpaq.org/@ojrock	Good
+// https://beamship.mpaq.org/@admin	Good
+//	https://mastodon.social/@gwaldby		BIG LIST!!! (3676/6466)
+
+}else{
+	$full=$_POST['D'];
+	$string=explode("@",$full);
+	$username=$string[1];
+	$instance=$string[0];
+	$userid=$instance."api/v1/accounts/lookup?acct=".$username;
+	$json=@file_get_contents($userid);
+	$user_data=json_decode($json);
+	if (isset($user_data)){
+		$user_id=$user_data->id;
+		$followers_count=$user_data->followers_count;
+		$following_count=$user_data->following_count;
+	}else{
+		echo "Unable to retrieve data for ".$_POST['D'];
+		exit;
+	}
+}
+if (!isset($user_id)){echo "Unknown System Error";exit;}
+$con = mysqli_connect($host_server,"mastodon","EndlessJourney","mastodon");
+$con->query("DROP TABLE IF EXISTS ".$user_id."_following;");
+$con->query("DROP TABLE IF EXISTS ".$user_id."_followers;");
+$following=$instance."api/v1/accounts/$user_id/following";
+$followers=$instance."api/v1/accounts/$user_id/followers";
+echo "Looking for ".$full." <A HREF='".$userid."' TARGET='_blank'>".$user_id."</A> 
+	<A HREF='".$following."' TARGET='_blank'>following</A> 
+	<A HREF='".$followers."' TARGET='_blank'>followers</A><BR>
+<FONT COLOR='green'>Green: Mutual</FONT>, <FONT COLOR='red'>Red: Not Mutual</FONT>, <FONT COLOR='black'>Black: Local</FONT><BR>";
+$output=shell_exec('/var/www/html/follow/list.sh '.$username.' '.$instance.' 2>&1');
+if (isset($output)){echo $output."<BR><BR>";}
+//include_once(FUNC.'/fun_comm_check.php');
+echo "<B>Who is following me?</B> ".$followers_count."<BR>";
+$sql="SELECT * FROM `".$user_id."_following` ORDER BY `name` ASC;";
+$res= $con->query($sql);
+while ($row = $res->fetch_assoc()){
+	$remote_user=$row['name'];
+	$s="SELECT * FROM `".$user_id."_followers` WHERE `name` LIKE '".$remote_user."';"; 
+	$r=$con->query($s);
+	$t = $r->fetch_assoc();
+	if (isset($row['name'])){
+		$name=explode('@',$row['name']);
+		if (isset($name[1])){
+			$remote="https://".$name[1];
+			if (!isset($t['name'])){
+				echo "<B><A HREF='".$instance."@".$name[0]."@".$name[1]."' TARGET='_blank' style='background-color:red;color: white;'>".$name[0]."</A></B> ";
+			}else{
+				echo "<A HREF='".$instance."@".$name[0]."@".$name[1]."' TARGET='_blank' style='background-color:green;color: white;'>".$name[0]."</A> ";
+			}
+		}else{
+			echo "<I><A HREF='".$instance."@".$row['name']."' TARGET='_blank' style='background-color:black;color: white;'>".$row['name']."</A></I> ";
+		}
+	}
+}
+echo "<BR><B>Who am I following?</B> ".$following_count."<BR>";
+$sql="SELECT * FROM `".$user_id."_following` ORDER BY `name` ASC;";
+$res= $con->query($sql);
+while ($row = $res->fetch_assoc()){
+	$remote_user=$row['name'];
+	$s="SELECT * FROM `".$user_id."_followers` WHERE `name` LIKE '".$remote_user."';"; 
+	$r=$con->query($s);
+	$t = $r->fetch_assoc();
+	if (isset($row['name'])){
+		$name=explode('@',$row['name']);
+		if (isset($name[1])){
+			$remote="https://".$name[1];
+			if (!isset($t['name'])){
+				echo "<B><A HREF='".$instance."@".$name[0]."@".$name[1]."' TARGET='_blank' style='background-color:red;color: white;'>".$name[0]."</A></B> ";
+			}
+			if (isset($t['name'])){
+				echo "<A HREF='".$instance."@".$name[0]."@".$name[1]."' TARGET='_blank' style='background-color:green;color: white;'>".$name[0]."</A> ";
+			}
+		}else{
+			echo "<I><A HREF='".$instance."@".$row['name']."' TARGET='_blank' style='background-color:black;color: white;'>".$row['name']."</A></I> ";
+		}
+	}
+}
+// RAILS_ENV=production bin/tootctl accounts modify admin --reset-password
+// admin	e47ec0de6f1e44944c3d8f2e435ae943
+// ./writefreely user reset-pass supadmin
+$con->close();
+?>

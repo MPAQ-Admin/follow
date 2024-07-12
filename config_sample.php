fbd8ec4d76bebb33bd8aef79863addda82ced91a
MPAQ-Admin <admin@mpaq.org>
2024-07-12 08:54:47 -0700

    Add files via upload

---
+++ config_sample.php
@@ -0,0 +1,10 @@
+<?PHP
+date_default_timezone_set('America/Chicago');
+$db=array(
+	"host"=>'localhost',
+	"user"=>'mastodon',
+	"passw"=>'mastodon',
+	"database"=>'mastodon'
+);
+$install_path="/path/to/folder/follow/";
+?>

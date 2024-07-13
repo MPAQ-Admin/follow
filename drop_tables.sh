98d638248f521571516fde26f7cb67dc8064ba3f
MPAQ-Admin <admin@mpaq.org>
2024-07-13 08:23:14 -0700

    Delete drop_tables.sh

--- drop_tables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DB_NAME="mastodon"
-DB_USER="mastodon"
-DB_PASSWORD="EndlessJourney"
-mysql -u $DB_USER -p$DB_PASSWORD -Nse "SHOW TABLES" $DB_NAME | while read table; do
-mysql -u $DB_USER -p$DB_PASSWORD -e "DROP TABLE IF EXISTS $table" $DB_NAME
-done
